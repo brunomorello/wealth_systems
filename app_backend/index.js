71d10011a24f2c5ee3c79877cb00a3c67932ef8d
brunomorello <brunomorello7@gamil.com>
2020-01-23 02:00:43 -0300

    get api finished

--- app_backend/index.js
+++ app_backend/index.js
@@ -1,6 +1,9 @@
 let app = require('./config/express.js')();
 let port = process.env.POST || 3000;
 const pool = require('./config/sql/postgres.js');
+const Farmer = require('./model/Farmer');
+const Address = require('./model/Address');
+const Document = require('./model/Document');
 
 app.get('/farmer/:searchable', (req, res) => {
 
@@ -8,8 +11,8 @@ app.get('/farmer/:searchable', (req, res) => {
 
     let query = `
         SELECT 
-            f.name, f.document as document_number, d.type as document_type,
-            a.street, a.address, a.state, a.country
+            f.id as farmer_id, f.name, f.document as document_number, d.type as document_type,
+            a.street, a.address, a.state, a.country, a.id as address_id
         FROM farmers f
         INNER JOIN addresses a on (f.address_id = a.id)
         INNER JOIN documents d on (f.document = d.number)
@@ -18,9 +21,25 @@ app.get('/farmer/:searchable', (req, res) => {
     `;
 
     pool.query(query, (error, results) => {
+        
         if(error) throw error;
 
-        res.json(results.rows);
+        let farmers = {
+            farmersList: []
+        };
+            
+
+        results.rows.forEach(row => {
+
+            var document = new Document(row.document_number, row.document_type);
+            var address = new Address(row.address_id, row.street, row.state, row.address, row.country);
+            var farmer = new Farmer(row.farmer_id, row.name, address, document);
+
+            farmers.farmersList.push(farmer);
+
+        });
+
+        res.json(farmers);
     })
 });
 
